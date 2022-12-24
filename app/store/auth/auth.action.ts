9111adea74813acb0e23747ff9668d163a31413f
Anton Kravkenko <antonkzavcenco300@gmail.com>
2022-12-24 21:43:11 +0100

    fix more big

--- app/store/auth/auth.action.ts
+++ app/store/auth/auth.action.ts
@@ -1,16 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import { useRouter } from 'next/router'
 import { toastr } from 'react-redux-toastr'
 import { IauthData, iUser } from '../../../shared/types/User.types'
 import { authServices } from '../../../utils/services/auth.services'
-
 
 export const register = createAsyncThunk<iUser, IauthData>('auth/register', async ({
                                                                                      email,
                                                                                      password,
                                                                                    }, thunkAPI) => {
   try {
+    const router = useRouter()
     const responce = await authServices.register(email, password)
     toastr.success('Register', 'access!')
+    router.push('/')
     return responce
   } catch (e) {
     toastr.error('Register', 'Error')
@@ -19,8 +21,10 @@ export const register = createAsyncThunk<iUser, IauthData>('auth/register', asyn
 })
 export const login = createAsyncThunk<iUser, IauthData>('auth/login', async ({ email, password }, thunkAPI) => {
   try {
+    const router = useRouter()
     const responce = await authServices.login(email, password)
     toastr.success('Login', 'access!')
+    router.push('/')
     return responce
   } catch (e) {
     toastr.error('Login', 'Error')
